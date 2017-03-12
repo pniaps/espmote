13283bd3c6d0ba68129b72d5b022d382b3f97014
Pedro Peña <pedro.sanchez@smartsoft.es>
2017-03-12 12:55:17 +0100

    Enable access point with button

--- Server.cpp
+++ Server.cpp
@@ -1,64 +1,312 @@
-/**
-   http://stackoverflow.com/a/28921280/2115411
-*/
-//function urldecode(s)
-//  s = s:gsub('+', ' ')
-//       :gsub('%%(%x%x)', function(h)
-//                           return string.char(tonumber(h, 16))
-//                         end)
-//  return s
-//end
-//
-//function parseurl(s)
-//  s = s:match('%s+(.+)')
-//  local ans = {}
-//  for k,v in s:gmatch('([^&=?]-)=([^&=?]+)' ) do
-//    ans[ k ] = urldecode(v)
-//  end
-//  return ans
-//end
-
 #include "Server.h"
 #define LED_PIN 2
 
-void checkPetition()
-{
-  // Check if a client has connected
-  WiFiClient client = server.available();
-  if (!client) {
-    return;
+int getRSSIasQuality(int RSSI) {
+  int quality = 0;
+
+  if (RSSI <= -100) {
+    quality = 0;
+  } else if (RSSI >= -50) {
+    quality = 100;
+  } else {
+    quality = 2 * (RSSI + 100);
   }
-
-  // Wait until the client sends some data
-  Serial.println("new client");
-  while (!client.available()) {
-    delay(1);
-  }
-
-  // Read the first line of the request
-  String req = client.readStringUntil('\r');
-  Serial.println(req);
-  client.flush();
-
-  String response = "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n";
-  // Match the request
-  if (req.indexOf("/enable") != -1) {
-    digitalWrite(LED_PIN, LOW);
-    response += "{\"enabled\":true}";
-  } else if (req.indexOf("/disable") != -1) {
-    digitalWrite(LED_PIN, HIGH);
-    response += "{\"enabled\":false}";
-  }else{
-    response += "{\"error\":\"invalid request\"}";
-  }
-
-  client.flush();
-
-  // Send the response to the client
-  client.print(response);
-  client.stop();
-  delay(1);
-  Serial.println("Client disonnected");
+  return quality;
 }
 
-WiFiServer server(80);
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += ( server.method() == HTTP_GET ) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+
+  for ( uint8_t i = 0; i < server.args(); i++ ) {
+    message += " " + server.argName ( i ) + ": " + server.arg ( i ) + "\n";
+  }
+
+  server.send ( 404, "text/plain", message );
+}
+
+void handleWifi(boolean scan) {
+
+  String page = FPSTR(HTTP_HEAD);
+  page.replace("{v}", "Config ESP");
+  page += FPSTR(HTTP_SCRIPT);
+  page += FPSTR(HTTP_STYLE);
+  //  page += _customHeadElement;
+  page += FPSTR(HTTP_HEAD_END);
+
+  if (scan) {
+    int n = WiFi.scanNetworks();
+    Serial.println(F("Scan done"));
+    if (n == 0) {
+      Serial.println(F("No networks found"));
+      page += F("No networks found. Refresh to scan again.");
+    } else {
+
+      //sort networks
+      int indices[n];
+      for (int i = 0; i < n; i++) {
+        indices[i] = i;
+      }
+
+      // RSSI SORT
+
+      // old sort
+      for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+          if (WiFi.RSSI(indices[j]) > WiFi.RSSI(indices[i])) {
+            std::swap(indices[i], indices[j]);
+          }
+        }
+      }
+
+      /*std::sort(indices, indices + n, [](const int & a, const int & b) -> bool
+        {
+        return WiFi.RSSI(a) > WiFi.RSSI(b);
+        });*/
+
+      //display networks in page
+      for (int i = 0; i < n; i++) {
+        if (indices[i] == -1) continue; // skip dups
+        Serial.println(WiFi.SSID(indices[i]));
+        Serial.println(WiFi.RSSI(indices[i]));
+        int quality = getRSSIasQuality(WiFi.RSSI(indices[i]));
+
+        String item = FPSTR(HTTP_ITEM);
+        String rssiQ;
+        rssiQ += quality;
+        item.replace("{v}", WiFi.SSID(indices[i]));
+        item.replace("{r}", rssiQ);
+        if (WiFi.encryptionType(indices[i]) != ENC_TYPE_NONE) {
+          item.replace("{i}", "l");
+        } else {
+          item.replace("{i}", "");
+        }
+        //Serial.println(item);
+        page += item;
+        delay(0);
+
+      }
+      page += "<br/>";
+    }
+  }
+
+  page += FPSTR(HTTP_FORM_START);
+  char parLength[2];
+  // add the extra parameters to the form
+  /*for (int i = 0; i < _paramsCount; i++) {
+    if (_params[i] == NULL) {
+      break;
+    }
+
+    String pitem = FPSTR(HTTP_FORM_PARAM);
+    if (_params[i]->getID() != NULL) {
+      pitem.replace("{i}", _params[i]->getID());
+      pitem.replace("{n}", _params[i]->getID());
+      pitem.replace("{p}", _params[i]->getPlaceholder());
+      snprintf(parLength, 2, "%d", _params[i]->getValueLength());
+      pitem.replace("{l}", parLength);
+      pitem.replace("{v}", _params[i]->getValue());
+      pitem.replace("{c}", _params[i]->getCustomHTML());
+    } else {
+      pitem = _params[i]->getCustomHTML();
+    }
+
+    page += pitem;
+    }
+    if (_params[0] != NULL) {
+    page += "<br/>";
+    }*/
+
+  /*if (_sta_static_ip) {
+
+    String item = FPSTR(HTTP_FORM_PARAM);
+    item.replace("{i}", "ip");
+    item.replace("{n}", "ip");
+    item.replace("{p}", "Static IP");
+    item.replace("{l}", "15");
+    item.replace("{v}", _sta_static_ip.toString());
+
+    page += item;
+
+    item = FPSTR(HTTP_FORM_PARAM);
+    item.replace("{i}", "gw");
+    item.replace("{n}", "gw");
+    item.replace("{p}", "Static Gateway");
+    item.replace("{l}", "15");
+    item.replace("{v}", _sta_static_gw.toString());
+
+    page += item;
+
+    item = FPSTR(HTTP_FORM_PARAM);
+    item.replace("{i}", "sn");
+    item.replace("{n}", "sn");
+    item.replace("{p}", "Subnet");
+    item.replace("{l}", "15");
+    item.replace("{v}", _sta_static_sn.toString());
+
+    page += item;
+
+    page += "<br/>";
+    }*/
+
+  page += FPSTR(HTTP_FORM_END);
+  page += FPSTR(HTTP_SCAN_LINK);
+
+  page += FPSTR(HTTP_END);
+
+  server.send(200, "text/html", page);
+
+
+  Serial.println(F("Sent config page"));
+}
+
+/** Handle the WLAN save form and redirect to WLAN config page again */
+void handleWifiSave() {
+  Serial.println(F("WiFi save"));
+
+  //SAVE/connect here
+  //  _ssid = server->arg("s").c_str();
+  //  _pass = server->arg("p").c_str();
+
+  //parameters
+  /*for (int i = 0; i < _paramsCount; i++) {
+    if (_params[i] == NULL) {
+      break;
+    }
+    //read parameter
+    String value = server->arg(_params[i]->getID()).c_str();
+    //store it in array
+    value.toCharArray(_params[i]->_value, _params[i]->_length);
+    Serial.println(F("Parameter"));
+    Serial.println(_params[i]->getID());
+    Serial.println(value);
+    }*/
+
+  /*if (server->arg("ip") != "") {
+    Serial.println(F("static ip"));
+    Serial.println(server->arg("ip"));
+    //_sta_static_ip.fromString(server->arg("ip"));
+    String ip = server->arg("ip");
+    optionalIPFromString(&_sta_static_ip, ip.c_str());
+    }
+    if (server->arg("gw") != "") {
+    Serial.println(F("static gateway"));
+    Serial.println(server->arg("gw"));
+    String gw = server->arg("gw");
+    optionalIPFromString(&_sta_static_gw, gw.c_str());
+    }
+    if (server->arg("sn") != "") {
+    Serial.println(F("static netmask"));
+    Serial.println(server->arg("sn"));
+    String sn = server->arg("sn");
+    optionalIPFromString(&_sta_static_sn, sn.c_str());
+    }*/
+
+  String page = FPSTR(HTTP_HEAD);
+  page.replace("{v}", "Credentials Saved");
+  page += FPSTR(HTTP_SCRIPT);
+  page += FPSTR(HTTP_STYLE);
+  //  page += _customHeadElement;
+  page += FPSTR(HTTP_HEAD_END);
+  page += FPSTR(HTTP_SAVED);
+  page += FPSTR(HTTP_END);
+
+  server.send(200, "text/html", page);
+
+  Serial.println(F("Sent wifi save page"));
+}
+
+
+void setupServer()
+{
+  server.on("/enable", []() {
+    digitalWrite(LED_PIN, LOW);
+    server.send ( 200, "application/json", "{\"enabled\":true}");
+  });
+
+  server.on("/disable", []() {
+    digitalWrite(LED_PIN, HIGH);
+    server.send ( 200, "application/json", "{\"enabled\":false}");
+  });
+
+  server.on("/status", []() {
+    String message = "";
+    if (digitalRead(LED_PIN) == LOW) {
+      message = "{\"enabled\":true}";
+    } else {
+      message = "{\"enabled\":false}";
+    }
+    server.send ( 200, "application/json", message);
+  });
+
+  server.on("/wifi", std::bind(&handleWifi, true));
+  server.on("/0wifi", std::bind(&handleWifi, false));
+  server.on("/wifisave", std::bind(&handleWifiSave));
+
+  server.on("/scan", []() {
+    String response = "{";
+    int n = WiFi.scanNetworks();
+    response = "{\"total\":";
+    response += n;
+    response += ",\"networks\":[";
+    if (n == 0) {
+//      Serial.println(F("No networks found"));
+//      page += F("No networks found. Refresh to scan again.");
+    } else {
+
+      //sort networks
+      int indices[n];
+      for (int i = 0; i < n; i++) {
+        indices[i] = i;
+      }
+
+      // RSSI SORT
+
+      // old sort
+      for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+          if (WiFi.RSSI(indices[j]) > WiFi.RSSI(indices[i])) {
+            std::swap(indices[i], indices[j]);
+          }
+        }
+      }
+
+      /*std::sort(indices, indices + n, [](const int & a, const int & b) -> bool
+        {
+        return WiFi.RSSI(a) > WiFi.RSSI(b);
+        });*/
+
+      //display networks in page
+      for (int i = 0; i < n; i++) {
+        if (indices[i] == -1) continue; // skip dups
+        if(i){
+          response += ",";
+        }
+        response += "{\"ssid\":\"";
+        response += WiFi.SSID(indices[i]);
+        response += "\",\"quality\":";
+        response += getRSSIasQuality(WiFi.RSSI(indices[i]));
+        response += ",\"encrypted\":";
+        response += (WiFi.encryptionType(indices[i]) != ENC_TYPE_NONE ? "true" : "false");
+        response += "}";
+        delay(0);
+      }
+      response += "]}";
+
+      server.send ( 200, "application/json", response);
+    }
+  });
+
+  server.onNotFound ( handleNotFound );
+
+  server.begin();
+
+}
+
+ESP8266WebServer server (80);
--- espmote.ino
+++ espmote.ino
@@ -3,48 +3,135 @@
 #include "OTA.h"
 #include "Server.h"
 
-#define GPIO_PIN 0 
+#define GPIO_PIN 0
 #define LED_PIN 2
+#define VERSION 170305
 
-const char* ssid     = "Einp";
-const char* password = "968208854";
+#include <Ticker.h>
+Ticker ticker;
+
+//#include <WiFiManager.h>
+
+//const char* ssid     = "Einp";
+//const char* password = "968208854";
 char hostString[16] = {0};
+
+//gets called when WiFiManager enters configuration mode
+//void configModeCallback (WiFiManager *myWiFiManager) {
+//  Serial.println("Entered config mode");
+//  Serial.println(WiFi.softAPIP());
+//  if you used auto generated SSID, print it
+//  Serial.println(myWiFiManager->getConfigPortalSSID());
+//  entered config mode, make led toggle faster
+//  ticker.attach(0.2, changeLED);
+//}
+
+void enableAP()
+{
+  //create AP with password
+  Serial.print("Setting soft-AP ... ");
+  Serial.print(WiFi.hostname().c_str());
+  Serial.println(WiFi.softAP("NUEVO", "12345678") ? " Ready" : " Failed!");
+
+  delay(500); // Without delay I've seen the IP address blank
+  Serial.print(F("Started access point. IP address: "));
+  Serial.println(WiFi.softAPIP());
+}
+
+void disableAP()
+{
+}
 
 void setup()
 {
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, HIGH);
-  
+  ticker.attach(0.2, changeLED);
+
   Serial.begin(115200);
-  Serial.println("setup");
+  Serial.println();
+  Serial.println();
 
-  Serial.println("\r\n");
-  Serial.print("Chip ID: 0x");
-  Serial.println(ESP.getChipId(), HEX);
-  Serial.print("FlashChipId: 0x");
-  Serial.println(ESP.getFlashChipId(), HEX);
-  Serial.print("FlashChipSize: ");
-  Serial.println(ESP.getFlashChipSize());
-  Serial.print("tFlashChipRealSize: ");
-  Serial.println(ESP.getFlashChipRealSize());
 
-  //Generate Hostname
+  //  WiFi.persistent(false); // Do not use SDK storage of SSID/WPA parameters
+
+  char hostString[16] = {0};
   sprintf(hostString, "ESP_%06X", ESP.getChipId());
-  Serial.print("Hostname: ");
-  Serial.println(hostString);
   WiFi.hostname(hostString);
 
-  //Connect to wifi
-  WiFi.begin(ssid, password);
+  // setup ssid for AP Mode when needed
+  WiFi.softAP(hostString, "12345678");
+  // We start in STA mode
+  WiFi.mode(WIFI_STA);
+
+
+  unsigned long _configPortalStart = millis();
+  Serial.println("Connecting to station ...");
+  WiFi.begin("Einp", "968208854");
   while (WiFi.status() != WL_CONNECTED) {
     delay(250);
     Serial.print(".");
+    if (millis() > _configPortalStart + 10000) {
+      //tiemout de 5 segundos
+      break;
+    }
   }
   Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  if (millis() > _configPortalStart + 10000) {
+    enableAP();
+  } else {
+    Serial.print("Connected to ");
+    Serial.println(WiFi.SSID());
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+  }
+
+  //  Serial.println("\n");
+  //  Serial.println("\n");
+  //  Serial.println("setup");
+  //  Serial.print("Version: ");
+  //  Serial.print(VERSION);
+  //  Serial.print(" ");
+  //Serial.print(__DATE__);
+  //Serial.println("\n");
+  //Serial.print("Chip ID: 0x");
+  //Serial.println(ESP.getChipId(), HEX);
+  //Serial.print("FlashChipId: 0x");
+  //Serial.println(ESP.getFlashChipId(), HEX);
+  //Serial.print("FlashChipSize: ");
+  //  Serial.println(ESP.getFlashChipSize());
+  //  Serial.print("aFlashChipRealSize: ");
+  //  Serial.println(ESP.getFlashChipRealSize());
+  //  Serial.print("getFlashChipSizeByChipId: 0x");
+  //  Serial.println(ESP.getFlashChipSizeByChipId(), HEX);
+
+  //Generate Hostname
+  //  sprintf(hostString, "ESP_%06X", ESP.getChipId());
+  //  Serial.print("Hostname => ");
+  //  Serial.println(WiFi.hostname().c_str());
+  //  WiFi.hostname(hostString);
+
+  //  WiFiManager wifiManager;
+  //  wifiManager.setAPCallback(configModeCallback);
+  //  if (!wifiManager.autoConnect()) {
+  //    Serial.println("failed to connect and hit timeout");
+  //reset and try again, or maybe put it to deep sleep
+  //    ESP.reset();
+  //    delay(1000);
+  //  }
+
+
+  //Connect to wifi
+  //  WiFi.begin(ssid, password);
+  //  while (WiFi.status() != WL_CONNECTED) {
+  //    delay(250);
+  //    Serial.print(".");
+  //  }
+  //  Serial.println("");
+  //  Serial.print("Connected to ");
+  //  Serial.println(WiFi.SSID());
+  //  Serial.print("IP address: ");
+  //  Serial.println(WiFi.localIP());
 
   //enable MDNS
   if (!MDNS.begin(hostString)) {
@@ -57,17 +144,59 @@ void setup()
   OTAConfigure();
 
   // Start the server
-  server.begin();
+  setupServer();
   Serial.println("Server started");
 
   // Turn led off and set interrupt
   pinMode(GPIO_PIN, INPUT);
-  attachInterrupt(GPIO_PIN, changeLED, FALLING);
+  attachInterrupt(GPIO_PIN, buttonChange, CHANGE);
+
+  ticker.detach();
+  digitalWrite(BUILTIN_LED, LOW);
 }
 
-void changeLED(){
-  digitalWrite(2, !digitalRead(2));
-  Serial.println("buttonChange");
+void changeLED() {
+  digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+}
+
+int buttonState = 0;     // current state of the button
+int startPressed = 0;    // the time button was pressed
+int endPressed = 0;      // the time button was released
+int timeHold = 0;        // the time button is hold
+int timeReleased = 0;    // the time button is released
+
+void buttonChange() {
+  delay(250);
+
+  buttonState = digitalRead(GPIO_PIN);
+  if (buttonState == LOW) {
+    startPressed = millis();
+    Serial.println("Button pressed");
+  } else {
+    endPressed = millis();
+    timeHold = endPressed - startPressed;
+    Serial.println("Button released");
+
+    if (timeHold >= 3000) {
+      //change configuration portal
+      Serial.println("Button hold for three seconds or more");
+      bool isEnabled = ((WiFi.getMode() & WIFI_AP) != 0);
+      if (isEnabled) {
+        Serial.print("Disabling soft-AP ... ");
+        Serial.println(WiFi.enableAP(false) ? " OK" : " Failed!");
+        delay(500);
+      } else {
+        Serial.print("Enabling soft-AP ... ");
+        //        Serial.println(WiFi.enableAP(true) ? " OK" : " Failed!");
+        Serial.print(WiFi.hostname().c_str());
+        Serial.println(WiFi.softAP(WiFi.hostname().c_str(), "12345678") ? " Ready" : " Failed!");
+        delay(500);
+      }
+    } else {
+      Serial.println("Button hold for no mor than three seconds");
+    }
+
+  }
 }
 
 
@@ -75,5 +204,5 @@ void loop()
 {
   ArduinoOTA.handle();
 
-  checkPetition();
+  server.handleClient();
 }
